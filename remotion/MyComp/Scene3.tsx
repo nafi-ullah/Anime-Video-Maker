5441f57054af7d284239dd31cd22ed3692505c7b
nafi-ullah <shafinnafiullah@gmail.com>
2024-02-11 18:13:59 +0600

    styled component added

--- remotion/MyComp/Scene3.tsx
+++ remotion/MyComp/Scene3.tsx
@@ -1,4 +1,13 @@
 import {AbsoluteFill } from 'remotion';
+import styled from 'styled-components';
+
+const Circle = styled.div`
+  width: 500px;
+  height: 500px;
+  border-radius: 250px;
+  background-color: white;
+`;
+
 
 export const Scene3: React.FC = () => {
 
@@ -9,6 +18,9 @@ export const Scene3: React.FC = () => {
         backgroundColor: '#4e00f9',
       }}
     >
+      {/* Circle animation lagbe mathay rekeh design korte hobe, chhoto theke boro hoy circle */}
+
+      <Circle></Circle>
      
       </AbsoluteFill>
   );

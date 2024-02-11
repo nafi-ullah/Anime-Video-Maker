89e7d4e276d0424425b33e53e0d53a059e39b3fa
nafi-ullah <shafinnafiullah@gmail.com>
2024-02-11 17:54:09 +0600

    nice transient

--- remotion/MyComp/Gradient.tsx
+++ remotion/MyComp/Gradient.tsx
@@ -1,10 +1,28 @@
-import {AbsoluteFill} from 'remotion';
+import {AbsoluteFill, useCurrentFrame, useVideoConfig} from 'remotion';
 
 export const Gradient: React.FC = () => {
+
+  const frame = useCurrentFrame(); // get current frame
+  const {height} = useVideoConfig(); // get height of the composition
+  const duration = 4 * 30; // 4 sec to spent through the whole gradient
+  const offset = height * 3 * (frame/duration);
+
   return (
+    <AbsoluteFill>
       <AbsoluteFill style={{
-        background: 'linear-gradient(to bottom, red, blue)'
-      }}>
+        
+        height: height * 1.5, // normal height er ektu beshi height jeno ektu pore seta normal a transform hoy, etai animation
+        background: 'linear-gradient(to bottom, #92a77d, #ccd56a, #e6b417, #e37e10, #dc2407, #470905, #090416, #2f0199, #736bdb, #adc1d3, #aecdbf, #92a77d)',
+        transform: `translateY(-${offset}px)`,
+      }}/>
+
+    <AbsoluteFill style={{
+        
+        height: height * 1.5, // normal height er ektu beshi height jeno ektu pore seta normal a transform hoy, etai animation
+        top: height * 1.5,
+        background: 'linear-gradient(to bottom, #92a77d, #ccd56a, #e6b417, #e37e10, #dc2407, #470905, #090416, #2f0199, #736bdb, #adc1d3, #aecdbf, #92a77d)',
+        transform: `translateY(-${offset}px)`,
+      }}/>
       </AbsoluteFill>
   );
 };
